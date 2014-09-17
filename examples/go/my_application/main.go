af1b9741c36a3082dffe942487ea5bc8c17f618a
Sig Lange <sig.lange@gmail.com>
2014-09-17 10:26:56 -0700

    touch up

--- examples/go/my_application/main.go
+++ examples/go/my_application/main.go
@@ -7,7 +7,7 @@ func main() {
    hello := 0
    for {
       hello++
-      fmt.Printf("I've said hello %d times\n", hello)
+      fmt.Printf("I've said hello %d time(s)\n", hello)
       time.Sleep(2 * time.Second)
    }
 }
