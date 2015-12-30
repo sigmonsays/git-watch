c0d3758ed1922f60d4afd4d5541a7f5fcf5c7269
Sig Lange <sig.lange@gmail.com>
2015-12-30 10:43:15 -0800

    gofmt

--- cmd/git-watch/config.go
+++ cmd/git-watch/config.go
@@ -1,78 +1,84 @@
 package main
+
 import (
-   "os"
-   "fmt"
-   "bytes"
-   "launchpad.net/goyaml"
+	"bytes"
+	"fmt"
+	"launchpad.net/goyaml"
+	"os"
 )
 
 type GitWatchConfig struct {
-   CheckInterval int
+	LogLevel      string
+	CheckInterval int
 
-   Dir string
-   Env []string
+	Dir string
+	Env []string
 
-   ExecCmd string
-   UpdateCmd string
-   InstallCmd string
-   LocalBranch string
+	GitRepos []string
+
+	InheritEnv bool
+
+	ExecCmd     string
+	UpdateCmd   string
+	InstallCmd  string
+	LocalBranch string
 }
+
 func DefaultGitWatchConfig() *GitWatchConfig {
-   return &GitWatchConfig{
-      CheckInterval: 1,
-      LocalBranch: "master",
-   }
+	return &GitWatchConfig{
+		CheckInterval: 1,
+		LocalBranch:   "master",
+	}
 }
 
 func (cfg *GitWatchConfig) FromFile(path string) error {
-   err := cfg.LoadYaml(path)
-   return err
+	err := cfg.LoadYaml(path)
+	return err
 }
 
 func (c *GitWatchConfig) LoadYaml(path string) error {
-    f, err := os.Open(path)
-    if err != nil {
-        return err
-    }
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
 
-    b := bytes.NewBuffer(nil)
-    _, err = b.ReadFrom(f)
-    if err != nil {
-        return err
-    }
+	b := bytes.NewBuffer(nil)
+	_, err = b.ReadFrom(f)
+	if err != nil {
+		return err
+	}
 
-    if err := c.LoadYamlBuffer(b.Bytes()); err != nil  {
-        return err
-    }
+	if err := c.LoadYamlBuffer(b.Bytes()); err != nil {
+		return err
+	}
 
-    if err := c.FixupConfig(); err != nil {
-        return err
-    }
+	if err := c.FixupConfig(); err != nil {
+		return err
+	}
 
-
-    return nil
+	return nil
 }
 func (c *GitWatchConfig) LoadYamlBuffer(buf []byte) error {
-    err := goyaml.Unmarshal(buf, c)
-    if err != nil {
-        return err
-    }
-    return nil
+	err := goyaml.Unmarshal(buf, c)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (conf *GitWatchConfig) PrintConfig() {
-    d, err := goyaml.Marshal(conf)
-    if err != nil {
-        fmt.Println("Marshal error", err)
-        return
-    }
-    fmt.Println(string(d))
+	d, err := goyaml.Marshal(conf)
+	if err != nil {
+		fmt.Println("Marshal error", err)
+		return
+	}
+	fmt.Println(string(d))
 }
 
 func (c *GitWatchConfig) FixupConfig() error {
 
-   if len(c.Env) == 0 {
-      c.Env = os.Environ()
-   }
-   return nil
+	if len(c.Env) == 0 {
+		c.Env = os.Environ()
+	}
+	return nil
 }
--- examples/go/my_application/main.go
+++ examples/go/my_application/main.go
@@ -1,18 +1,20 @@
 package main
-import (
-   "fmt"
-   "time"
-)
-func main() {
-   hello := 0
-   for {
-      hello++
-      fmt.Printf("I've said hello %d time(s)\n", hello)
-      time.Sleep(2 * time.Second)
 
-      if hello == 100 {
-         fmt.Println("I'm tired, goodbye")
-         break
-      }
-   }
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	hello := 0
+	for {
+		hello++
+		fmt.Printf("I've said hello %d time(s)\n", hello)
+		time.Sleep(2 * time.Second)
+
+		if hello == 100 {
+			fmt.Println("I'm tired, goodbye")
+			break
+		}
+	}
 }
